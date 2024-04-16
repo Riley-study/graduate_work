3a75b82635c522215a12ec755ad3c273ad6e3a48
Riley-study <120293475+Riley-study@users.noreply.github.com>
2024-04-16 20:20:58 +0500

    templates added

--- analytic_app/urls.py
+++ analytic_app/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index, sales, costs, product_range, profitability, forecasts, indicators_by_point_of_sale
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('sales/', sales, name='sales'),
+    path('costs/', costs, name='costs'),
+    path('product_range/', product_range, name='product_range'),
+    path('profitability/', profitability, name='profitability'),
+    path('forecasts/', forecasts, name='forecasts'),
+    path('indicators_by_point_of_sale/', indicators_by_point_of_sale, name='indicators_by_point_of_sale'),
 ]
--- analytic_app/views.py
+++ analytic_app/views.py
@@ -3,4 +3,28 @@ from django.http import HttpResponse
 
 
 def index(request):
- return render(request, 'main_page.html')
+    return render(request, 'analytic_app/main_page.html')
+
+
+def sales(request):
+    return render(request, 'analytic_app/sales.html')
+
+
+def costs(request):
+    return render(request, 'analytic_app/costs.html')
+
+
+def product_range(request):
+    return render(request, 'analytic_app/product_range.html')
+
+
+def profitability(request):
+    return render(request, 'analytic_app/profitability.html')
+
+
+def forecasts(request):
+    return render(request, 'analytic_app/forecasts.html')
+
+
+def indicators_by_point_of_sale(request):
+    return render(request, 'analytic_app/indicators_by_point_of_sale.html')
