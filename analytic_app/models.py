5188dbbef7a7bfae77466b7c397f45a7873e8f6d
Riley-study <120293475+Riley-study@users.noreply.github.com>
2024-04-16 20:21:28 +0500

    models added

--- analytic_app/models.py
+++ analytic_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Bakery_address(models.Model):
+    bakery_address = models.CharField(max_length=100)
+
+
+class Profitability_by_product(models.Model):
+    product_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    bakery_address = models.ForeignKey(Bakery_address, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    total_sum = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class Costs_by_month(models.Model):
+    cost_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    bakery_address = models.ForeignKey(Bakery_address, on_delete=models.CASCADE)
+    amount_of_costs = models.DecimalField(max_digits=8, decimal_places=2)
